87ad4c1cf8c91229ee046ec83cc4983868152102
Louy Alsadi <louyalsadi@Louys-MacBook-Pro.local>
2020-07-08 14:56:11 -0500

    Use Abstract class

---
+++ Record.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace PhoneBookAbstract
+{
+    static class Record
+    {
+        static int counter = 0;
+        public static string CreateRecord(int id, string name, string number)
+        {
+            counter++;
+            return id + "-" + " " + name + " " + number ;
+        }
+    }
+}
